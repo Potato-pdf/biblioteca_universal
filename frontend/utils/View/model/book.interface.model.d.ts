050e019a6c7b4bec4a76127565ba4a5e1214d583
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-19 12:12:05 -0600

    bookVIewMOdel

---
+++ frontend/utils/View/model/book.interface.model.d.ts
@@ -0,0 +1,9 @@
+export interface LibroViewModel {
+    id: number;
+    name: string;
+    imageUrl: string;
+    pdfUrl: string;
+    authorName: string;
+    description: string;
+    publishDate: string;
+}
