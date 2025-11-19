0e1dd9e4061685b43b676c25cb416ffbe8d0b7a6
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-19 12:51:34 -0600

    adddata to user model

--- backend/src/domain/models/users/user.model.ts
+++ backend/src/domain/models/users/user.model.ts
@@ -1,16 +1,16 @@
 import { Column, Entity, PrimaryColumn } from "typeorm";
 
-@Entity
-export class User{
-    @PrimaryColumn()
-    id! :string
-    
-    @Column()
-    name!: string
-    
-    @Column()
-    rol!: string
+@Entity()
+export class User {
+    @PrimaryColumn("varchar", { length: 36 })
+    id!: string;
 
-    @Column({ unique: true })
-    email!:string
+    @Column("varchar", { length: 255 })
+    name!: string;
+
+    @Column("varchar", { length: 50 })
+    rol!: string;
+
+    @Column("varchar", { length: 255, unique: true })
+    email!: string;
 }
