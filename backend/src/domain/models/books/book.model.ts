49ebf75cb53cc9c44ba9e9740c8bdd6ec565fe90
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-19 12:51:23 -0600

    feat: adda data to book model

--- backend/src/domain/models/books/book.model.ts
+++ backend/src/domain/models/books/book.model.ts
@@ -1,26 +1,25 @@
 import { Column, Entity, PrimaryColumn } from "typeorm";
 
+@Entity()
+export class Book {
+    @PrimaryColumn("varchar", { length: 36 })
+    id!: string;
 
-@Entity
-export class Book{
-    @PrimaryColumn()
-    id! :string
+    @Column("varchar", { length: 500 })
+    name!: string;
 
-    @Column()
-    name!:string
-    
-    @Column()
-    imageUrl!:string
-    
-    @Column()
-    authorName!:string
-    
-    @Column()
-    pdfUrl! : string 
-    
-    @Column()
-    description!: string
-    
-    @Column()
-    publishDate!:string
+    @Column("text")
+    imageUrl!: string;
+
+    @Column("varchar", { length: 255 })
+    authorName!: string;
+
+    @Column("text")
+    pdfUrl!: string;
+
+    @Column("text")
+    description!: string;
+
+    @Column("varchar", { length: 50 })
+    publishDate!: string;
 }
